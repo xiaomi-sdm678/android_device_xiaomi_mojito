4cdbf534b3bd012b21778ec40b6ef1754b3083a4
Bruno Martins <bgcngm@gmail.com>
2020-10-09 12:19:37 +0100

    mojito: power: Adapt extension to new aidl power HAL
    Change-Id: I58ac49957d8d28ad904912986d89ac8db4820ff9
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -143,7 +143,7 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 
 # Power
-TARGET_POWER_SET_FEATURE_LIB := libpower_feature.mojito
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
