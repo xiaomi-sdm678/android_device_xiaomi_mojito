bafdde8c6faf990f0ff0faedb0655a91752aebe8
raghavt20 <raghavtandon2000@gmail.com>
2020-11-16 15:56:04 +0530

    violet: Configure board specific platform
    Change-Id: Ia75844d6c722bb9287a9825f25f71d5acf3a4db9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,5 +11,9 @@ DEVICE_PATH := device/xiaomi/violet
 # Inherit from sm6150-common
 -include device/xiaomi/sm6150-common/BoardConfigCommon.mk
 
+# Platform
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno612
+
 # Inherit from proprietary files
 include vendor/xiaomi/violet/BoardConfigVendor.mk
+
