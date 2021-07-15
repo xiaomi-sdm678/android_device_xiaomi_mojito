4cdc30875eab45be598bff331e3a867499ef0905
dataoutputstream <mb150996@gmail.com>
2021-07-15 15:29:41 +0200

    mojito: Import bootctrl and gpt_utils hal
    Change-Id: Iefe7f59e28ebb667f8bbbf096b38923c08b863db
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,23 @@ include device/xiaomi/sm6150-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/mojito
 
+# A/B
+BOARD_USES_RECOVERY_AS_BOOT := true
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    odm \
+    product \
+    system \
+    system_ext \
+    vbmeta \
+    vbmeta_system \
+    vendor \
+    vendor_boot
+
 # Board
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 
