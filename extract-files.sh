41cbc1cb36a8f15afdc4ac30228f3ef980b7e8a3
Anush02198 <Anush.4376@gmail.com>
2021-08-01 17:53:53 +0530

    mojito:Initial Blob Pin

--- extract-files.sh
+++ extract-files.sh
@@ -1,6 +1,8 @@
+
 #!/bin/bash
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
