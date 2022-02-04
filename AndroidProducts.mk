dd0afce4cc67c1bbea72042815e56298cfa52572
romgharti <romgharti@gmail.com>
2022-02-04 21:31:40 +0545

    mojito: AospExtended Bringup
    Signed-off-by: romgharti <romgharti@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_mojito.mk
+    $(LOCAL_DIR)/aosp_mojito.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_mojito-eng \
-    lineage_mojito-userdebug \
-    lineage_mojito-eng
+    aosp_mojito-eng \
+    aosp_mojito-userdebug \
+    aosp_mojito-eng
